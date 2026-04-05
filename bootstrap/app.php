a02d7243e837334780b6be90a4d059c71a2b84cf
cahayaoptimakarya <cahayaoptimakaryaid@gmail.com>
2026-04-05 11:24:29 +0700

    test skrip trigger pengubah data

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Console\Commands\RecalculatePoLineFulfillment;
+use App\Console\Commands\MovePickingDate;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withCommands([
         RecalculatePoLineFulfillment::class,
+        MovePickingDate::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
