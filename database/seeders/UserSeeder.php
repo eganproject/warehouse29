eb742c48cd508276792a7ca31cc626a44163ddd0
Ega Nugraha <eganugrahaid@gmail.com>
2026-05-19 13:53:21 +0700

    ubah seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,13 +10,13 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Create or update admin user.
+        // Create or update superadmin user.
         DB::table('users')->updateOrInsert(
             ['email' => 'superadmin@gmail.com'],
             [
                 'name' => 'Super Administrator',
                 'divisi_id' => $this->divisiId('tanpa divisi'),
-                'password' => Hash::make('Password29!2'),
+                'password' => Hash::make('Password!2'),
                 'email_verified_at' => now(),
                 'updated_at' => now(),
                 'created_at' => now(),
@@ -34,41 +34,14 @@ class UserSeeder extends Seeder
         }
 
         $users = [
-            ['name' => 'idris', 'email' => 'idris@gmail.com', 'password' => '567890', 'role' => 'Captain', 'divisi' => 'Akrilik'],
-            ['name' => 'nurfan', 'email' => 'nurfan@gmail.com', 'password' => '567891', 'role' => 'Captain', 'divisi' => 'Aksesoris'],
-            ['name' => 'Dalton', 'email' => 'daltonqc@gmail.com', 'password' => 'D09128', 'role' => 'QC Scan', 'divisi' => 'Akrilik'],
-            ['name' => 'Topic', 'email' => 'adminqc@gmail.com', 'password' => 'T01928', 'role' => 'QC Scan', 'divisi' => 'Akrilik'],
-            ['name' => 'Arya', 'email' => 'arya@gmail.com', 'password' => 'A123098', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Budi Mulia', 'email' => 'budi@gmail.com', 'password' => 'B345678', 'role' => 'Picker', 'divisi' => 'Akrilik'],
-            ['name' => 'Abduljaba', 'email' => 'abduljabar@gmail.com', 'password' => 'Aj90202', 'role' => 'Picker', 'divisi' => 'Akrilik'],
-            ['name' => 'Tezi Mei', 'email' => 'tezi@gmail.com', 'password' => 'T10987', 'role' => 'Picker', 'divisi' => 'Akrilik'],
-            ['name' => 'Suherman', 'email' => 'suherman@gmail.com', 'password' => 'S908291', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Ade', 'email' => 'ade@gmail.com', 'password' => 'AD71902', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Billy', 'email' => 'billy@gmail.com', 'password' => 'Bi091234', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'AZIZ FARID MAULANA', 'email' => 'aziz@gmail.com', 'password' => 'AZ12093487', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Arifilham', 'email' => 'arifilham@gmail.com', 'password' => 'Ar90879', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Riyandi Aprilip', 'email' => 'riyandi@gmail.com', 'password' => 'R5612980', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Riski', 'email' => 'riski@gmail.com', 'password' => 'RI1209873', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'Bily', 'email' => 'bily@gmail.com', 'password' => 'B92831', 'role' => 'Packer', 'divisi' => 'Akrilik'],
-            ['name' => 'M  Chotib', 'email' => 'chotib@gmail.com', 'password' => 'CH128901', 'role' => 'QC Scan', 'divisi' => 'Akrilik'],
-            ['name' => 'Lupi', 'email' => 'lupi@gmail.com', 'password' => 'L019191', 'role' => 'Picker', 'divisi' => 'Aksesoris'],
-            ['name' => 'Pirman', 'email' => 'pirman@gmail.com', 'password' => 'Pir83012', 'role' => 'Packer', 'divisi' => 'Aksesoris'],
-            ['name' => 'Pathur', 'email' => 'pathur@gmail.com', 'password' => 'Pat23910', 'role' => 'Packer', 'divisi' => 'Aksesoris'],
-            ['name' => 'Dika', 'email' => 'dika@gmail.com', 'password' => 'D36781', 'role' => 'QC Scan', 'divisi' => 'Aksesoris'],
-            ['name' => 'Angga', 'email' => 'angga@gmail.com', 'password' => 'ANG7810', 'role' => 'Packer', 'divisi' => 'Aksesoris'],
-            ['name' => 'Muhammad Rizky', 'email' => 'rizky@gmail.com', 'password' => 'RIZ672192', 'role' => 'Picker', 'divisi' => 'Aksesoris'],
-            ['name' => 'Rasya', 'email' => 'rasya@gmail.com', 'password' => 'RAS8273', 'role' => 'Picker', 'divisi' => 'Aksesoris'],
-            ['name' => 'Elsa Lintang', 'email' => 'elsa@gmail.com', 'password' => 'ELS20987', 'role' => 'Picker', 'divisi' => 'Aksesoris'],
-            ['name' => 'Trio', 'email' => 'trio@gmail.com', 'password' => 'TR409123', 'role' => 'QC Scan', 'divisi' => 'Aksesoris'],
-            ['name' => 'Fikih', 'email' => 'fikih@gmail.com', 'password' => 'FIK012381', 'role' => 'Packer', 'divisi' => 'Aksesoris'],
-            ['name' => 'Indra', 'email' => 'indra@gmail.com', 'password' => 'In567391', 'role' => 'Admin retur', 'divisi' => null],
-            ['name' => 'Badru', 'email' => 'badru@gmail.com', 'password' => 'B1234567', 'role' => 'Kepala Gudang', 'divisi' => null],
-            ['name' => 'Gabriel', 'email' => 'gabriel@gmail.com', 'password' => 'Gab123989', 'role' => 'Admin Gudang', 'divisi' => null],
-            ['name' => 'Naufal', 'email' => 'naufal@gmail.com', 'password' => 'N12345', 'role' => 'Admin Resi', 'divisi' => null],
-            ['name' => 'Syahrul', 'email' => 'syahrul@gmail.com', 'password' => 'S12890', 'role' => 'Scan Out', 'divisi' => null],
-            ['name' => 'Kosim', 'email' => 'kosim@gmail.com', 'password' => 'K82729', 'role' => 'Scan Out', 'divisi' => null],
-            ['name' => 'Seken', 'email' => 'seken@gmail.com', 'password' => '288931', 'role' => 'Scan Out', 'divisi' => null],
-            ['name' => 'Fahri', 'email' => 'fahri@gmail.com', 'password' => 'F019283', 'role' => 'QC Scan', 'divisi' => null],
+            ['name' => 'Captain',        'email' => 'captain@gmail.com',       'password' => 'Password!2', 'role' => 'captain'],
+            ['name' => 'QC Scan',        'email' => 'qcscan@gmail.com',        'password' => 'Password!2', 'role' => 'picker'],
+            ['name' => 'Packer',         'email' => 'packer@gmail.com',        'password' => 'Password!2', 'role' => 'packer'],
+            ['name' => 'Scan Out',       'email' => 'scanout@gmail.com',       'password' => 'Password!2', 'role' => 'admin-scan'],
+            ['name' => 'Admin Retur',    'email' => 'adminretur@gmail.com',    'password' => 'Password!2', 'role' => 'admin-retur'],
+            ['name' => 'Admin Resi',     'email' => 'adminresi@gmail.com',     'password' => 'Password!2', 'role' => 'admin-resi'],
+            ['name' => 'Kepala Gudang',  'email' => 'kepalagudang@gmail.com',  'password' => 'Password!2', 'role' => 'kepala-gudang'],
+            ['name' => 'Admin Gudang',   'email' => 'admingudang@gmail.com',   'password' => 'Password!2', 'role' => 'admin-gudang'],
         ];
 
         foreach ($users as $user) {
@@ -76,7 +49,7 @@ class UserSeeder extends Seeder
                 ['email' => $user['email']],
                 [
                     'name' => $user['name'],
-                    'divisi_id' => $this->divisiId($user['divisi']),
+                    'divisi_id' => $this->divisiId('tanpa divisi'),
                     'password' => Hash::make($user['password']),
                     'email_verified_at' => now(),
                     'updated_at' => now(),
@@ -86,7 +59,7 @@ class UserSeeder extends Seeder
 
             $createdUser = DB::table('users')->where('email', $user['email'])->first();
             if ($createdUser) {
-                $this->syncRoles($createdUser->id, $this->roleSlugs($user['role']));
+                $this->syncRoles($createdUser->id, [$user['role']]);
             }
         }
     }
@@ -100,26 +73,6 @@ class UserSeeder extends Seeder
         return DB::table('divisis')
             ->whereRaw('LOWER(name) = ?', [strtolower(trim($name))])
             ->value('id');
-    }
-
-    /**
-     * The application still uses the legacy "picker" slug for QC/picking access.
-     *
-     * @return array<int,string>
-     */
-    private function roleSlugs(?string $role): array
-    {
-        return match (strtolower(trim((string) $role))) {
-            'captain' => ['captain'],
-            'qc scan', 'picker' => ['picker'],
-            'packer' => ['packer'],
-            'scan out' => ['admin-scan'],
-            'admin retur' => ['admin-retur'],
-            'admin resi' => ['admin-resi'],
-            'kepala gudang' => ['kepala-gudang'],
-            'admin gudang' => ['admin-gudang'],
-            default => [],
-        };
     }
 
     /**
